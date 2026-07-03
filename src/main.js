2e6c685e0c1561f881410cb6d8afec7591d34384
Sergio Shambir <realistic.sergio@gmail.com>
2026-07-03 23:32:02 +0200

    feat: setup project structure, add partials and legal pages

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import './js/burgerMenu';
+import './js/header';
+import './js/faq';
+import './js/slider';
+import './js/scroll';
+
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 30) {
+    header.classList.add('is-scrolled');
+  } else {
+    header.classList.remove('is-scrolled');
+  }
+});
